29ae886a55c5849b71e635b900ae437f854144f7
Mohammad Goli <gli.mhmd@gmail.com>
2017-04-02 00:58:19 +0430

    Lovely Startup

--- DirectoryStructure/project/songs/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
-
-
-class FormName(Form):
-    FieldName = FieldType(
-        'HTMLName',
-        validators=[DataRequired(), Email(), Length(min=num, max=num), EqualTo('HTMLNameP')])
